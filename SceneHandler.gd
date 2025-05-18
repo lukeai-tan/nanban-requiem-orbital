df13cccc57a0caaf91c19a766711efd10c9a35c1
lukeai-tan <lukeaidantan1612@gmail.com>
2025-05-18 20:26:12 +0800

    changes to main menu
    removed trophies button temporarily

--- SceneHandler.gd
+++ SceneHandler.gd
@@ -2,7 +2,7 @@ extends Node
 
 func _ready():
 	get_node("MainMenu/Margin/VBoxContainer/NewGame").connect("pressed", Callable(self, "onNewGamePressed"))
-	get_node("MainMenu/Margin/VBoxContainer/Trophies").connect("pressed", Callable(self, "onTrophiesPressed"))
+	# get_node("MainMenu/Margin/VBoxContainer/Trophies").connect("pressed", Callable(self, "onTrophiesPressed"))
 	get_node("MainMenu/Margin/VBoxContainer/Settings").connect("pressed", Callable(self, "onSettingsPressed"))
 	get_node("MainMenu/Margin/VBoxContainer/Credits").connect("pressed", Callable(self, "onCreditsPressed"))
 	get_node("MainMenu/Margin/VBoxContainer/Quit").connect("pressed", Callable(self, "onQuitPressed"))
@@ -12,9 +12,10 @@ func onNewGamePressed():
 	var gameScene = load("res://Scenes/MainScenes/GameScene.tscn").instantiate()
 	add_child(gameScene)
 	
+'''
 func onTrophiesPressed():
 	pass
-	
+''' 
 func onSettingsPressed():
 	pass
 	
