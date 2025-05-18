0859345109fa3133072bf581cc63b6631a14fe0b
lukeai-tan <lukeaidantan1612@gmail.com>
2025-05-19 00:08:34 +0800

    Update SceneHandler.gd
    changed from camelcase to snakecase

--- SceneHandler.gd
+++ SceneHandler.gd
@@ -1,26 +1,26 @@
 extends Node
 
 func _ready():
-	get_node("MainMenu/Margin/VB_buttons/NewGame").connect("pressed", Callable(self, "onNewGamePressed"))
-	# get_node("MainMenu/Margin/VB_buttons/Trophies").connect("pressed", Callable(self, "onTrophiesPressed"))
-	get_node("MainMenu/Margin/VB_buttons/Settings").connect("pressed", Callable(self, "onSettingsPressed"))
-	get_node("MainMenu/Margin/VB_buttons/Credits").connect("pressed", Callable(self, "onCreditsPressed"))
-	get_node("MainMenu/Margin/VB_buttons/Quit").connect("pressed", Callable(self, "onQuitPressed"))
+	get_node("MainMenu/Margin/VB_buttons/NewGame").connect("pressed", Callable(self, "on_new_game_pressed"))
+	# get_node("MainMenu/Margin/VB_buttons/Trophies").connect("pressed", Callable(self, "on_trophies_pressed"))
+	get_node("MainMenu/Margin/VB_buttons/Settings").connect("pressed", Callable(self, "on_settings_pressed"))
+	get_node("MainMenu/Margin/VB_buttons/Credits").connect("pressed", Callable(self, "on_credits_pressed"))
+	get_node("MainMenu/Margin/VB_buttons/Quit").connect("pressed", Callable(self, "on_quit_pressed"))
 
-func onNewGamePressed():
+func on_new_game_pressed():
 	get_node("MainMenu").queue_free()
 	var gameScene = load("res://Scenes/MainScenes/GameScene.tscn").instantiate()
 	add_child(gameScene)
 	
 '''
-func onTrophiesPressed():
+func on_trophies_pressed():
 	pass
 ''' 
-func onSettingsPressed():
+func on_settings_pressed():
 	pass
 	
-func onCreditsPressed():
+func on_credits_pressed():
 	pass
 
-func onQuitPressed():
+func on_quit_pressed():
 	get_tree().quit()
