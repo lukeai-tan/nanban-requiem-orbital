a5694ee7c921f06b9c9449d233d1e0eee389f085
lukeai-tan <lukeaidantan1612@gmail.com>
2025-05-19 19:39:06 +0800

    backtracked on the tower inheritance
    I put the tracking feature in RangedTower 1 and 2 due to some path issues with the inheritance file

--- Scenes/Towers/RangedTower2.gd
+++ Scenes/Towers/RangedTower2.gd
@@ -1,1 +1,10 @@
-extends "res://Scenes/Towers/TowerLogic.gd"
+#extends "res://Scenes/Towers/TowerLogic.gd"
+extends Node2D
+
+func _physics_process(_delta):
+	turn()
+
+# rotation of turrets on Ranged towers
+func turn():
+	var enemy_pos = get_global_mouse_position()
+	get_node("Turret").look_at(enemy_pos)
