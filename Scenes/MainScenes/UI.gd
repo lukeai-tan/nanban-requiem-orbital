35efa6cce6c5ca244f3aa3781f06f5766b4fc6f6
lukeai-tan <lukeaidantan1612@gmail.com>
2025-05-24 12:46:28 +0800

    added functionality to pause, play, fastforward

--- Scenes/MainScenes/UI.gd
+++ Scenes/MainScenes/UI.gd
@@ -29,3 +29,17 @@ func update_tower_preview(new_position, color):
 		get_node("TowerPreview/DragTower").modulate = Color(color)
 		# range indicator (attack radius)
 		get_node("TowerPreview/Sprite2D").modulate = Color(color)
+
+
+func _on_pause_play_pressed() -> void:
+	if get_tree().is_paused():
+		get_tree().paused = false
+	else:
+		get_tree().paused = true
+
+
+func _on_fast_forward_pressed() -> void:
+	if Engine.get_time_scale() == 10.0:
+		Engine.set_time_scale(1.0)
+	else:
+		Engine.set_time_scale(10.0)
