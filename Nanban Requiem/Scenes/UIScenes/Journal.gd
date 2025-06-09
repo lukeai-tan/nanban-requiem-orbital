90d4ace89d936d8bb0bc90b1103843cd2a36888f
lukeai-tan <lukeaidantan1612@gmail.com>
2025-06-09 20:50:22 +0800

    journal entry info panel can be exited

--- Nanban Requiem/Scenes/UIScenes/Journal.gd
+++ Nanban Requiem/Scenes/UIScenes/Journal.gd
@@ -40,3 +40,7 @@ func _on_icon_pressed(entry):
 func _on_back_to_main_pressed() -> void:
 	emit_signal("return_to_main_menu")
 	queue_free()
+
+
+func _on_close_panel_pressed() -> void:
+	info_panel.visible = false
