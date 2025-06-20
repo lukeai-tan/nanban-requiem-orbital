4bd9332f3682423205cbbb62e083d66642b114fa
lukeai-tan <lukeaidantan1612@gmail.com>
2025-06-21 01:17:31 +0800

    improved visuals for map selector

--- Nanban Requiem/Scenes/UIScenes/MapSelector.gd
+++ Nanban Requiem/Scenes/UIScenes/MapSelector.gd
@@ -8,16 +8,20 @@ var move_tween: Tween
 func _ready() -> void:
 	# sets default map to index 0 (aka Map 1)
 	$PlayerIcon.global_position = maps[current_map].global_position
+	maps[current_map].get_node("Label").self_modulate = Color(1, 1, 0)
+
 
 func _input(event):
 	if move_tween and move_tween.is_running():
 		return
 
 	if event.is_action_pressed("ui_left") and current_map > 0:
+		maps[current_map].get_node("Label").self_modulate = Color(1, 1, 1)
 		current_map -= 1
 		tween_icon()
 	
 	if event.is_action_pressed("ui_right") and current_map < maps.size() - 1:
+		maps[current_map].get_node("Label").self_modulate = Color(1, 1, 1)
 		current_map += 1
 		tween_icon()
 
@@ -34,4 +38,5 @@ func _input(event):
 
 func tween_icon():
 	move_tween = get_tree().create_tween()
-	move_tween.tween_property($PlayerIcon, "global_position", maps[current_map].global_position, 0.5).set_trans(Tween.TRANS_SINE)
+	move_tween.tween_property($PlayerIcon, "global_position", maps[current_map].global_position, 0.3).set_trans(Tween.TRANS_SINE)
+	maps[current_map].get_node("Label").self_modulate = Color(1, 1, 0)
