c181f413a739ef447d87de1129d8b4a19c4adf9d
lukeai-tan <lukeaidantan1612@gmail.com>
2025-06-21 00:51:54 +0800

    added tweens to map selector

--- Nanban Requiem/Scenes/UIScenes/MapSelector.gd
+++ Nanban Requiem/Scenes/UIScenes/MapSelector.gd
@@ -3,19 +3,23 @@ class_name MapSelector
 
 @onready var maps: Array = [$MapIcon, $MapIcon2, $MapIcon3]
 var current_map: int = 0
+var move_tween: Tween
 
 func _ready() -> void:
 	# sets default map to index 0 (aka Map 1)
 	$PlayerIcon.global_position = maps[current_map].global_position
 
 func _input(event):
+	if move_tween and move_tween.is_running():
+		return
+
 	if event.is_action_pressed("ui_left") and current_map > 0:
 		current_map -= 1
-		$PlayerIcon.global_position = maps[current_map].global_position
+		tween_icon()
 	
 	if event.is_action_pressed("ui_right") and current_map < maps.size() - 1:
 		current_map += 1
-		$PlayerIcon.global_position = maps[current_map].global_position
+		tween_icon()
 
 	if event.is_action_pressed("ui_accept"):
 		print("Map clicked")
@@ -28,3 +32,6 @@ func _input(event):
 			get_tree().get_root().remove_child(self)
 	'''
 
+func tween_icon():
+	move_tween = get_tree().create_tween()
+	move_tween.tween_property($PlayerIcon, "global_position", maps[current_map].global_position, 0.5).set_trans(Tween.TRANS_SINE)
