efd83c593af7dc29d3b13edebd30a3d87d85a599
lukeai-tan <lukeaidantan1612@gmail.com>
2025-07-14 23:48:57 +0800

    made towers clickable

--- Nanban Requiem/C# Classes/Towers (Base)/Tower.cs
+++ Nanban Requiem/C# Classes/Towers (Base)/Tower.cs
@@ -5,10 +5,13 @@ using Godot;
 // Layer 0 tower logic that handles hit response
 // Required fields: health, physDefense, artsDefense
 public abstract partial class Tower : Unit
-{
+{   
+
     public virtual void Signal(Enemy target)
     {
 
     }
+
+
     
 }
--- Nanban Requiem/C# Classes/Towers (Functional)/RangedTower1.cs
+++ Nanban Requiem/C# Classes/Towers (Functional)/RangedTower1.cs
@@ -15,4 +15,15 @@ public partial class RangedTower1 : RangedTowerBase
         return "Ranged Tower 1: " + this.attack.ToString() + " " + base.ToString();
     }
 
+    public override void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx)
+    {
+        if (@event is InputEventMouseButton mouseEvent)
+        {
+            if (mouseEvent.Pressed && mouseEvent.ButtonIndex == MouseButton.Left)
+            {
+                GD.Print($"Tower clicked!");
+            }
+        }
+    }
+
 }
