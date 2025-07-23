504f1d95e0447a30ed95d7dc274714b613b1e632
lukeai-tan <lukeaidantan1612@gmail.com>
2025-07-23 22:20:47 +0800

    added fourth wall

--- Nanban Requiem/Scenes/MainScenes/ChickenUI.gd
+++ Nanban Requiem/Scenes/MainScenes/ChickenUI.gd
@@ -1,8 +1,15 @@
 extends "res://Scenes/MainScenes/UI.gd"
 
+var fourth_wall: bool = false
 
-
+func _on_restart_pressed() -> void:
+    if !fourth_wall:
+        var label = quit_window.get_node("Label")
+        label.text += "\n" + UserInfo.username
+        fourth_wall = true
+    super._on_restart_pressed()
+    
 
 func _on_quit_button_pressed() -> void:
-	quit_window.visible = false
-	get_tree().paused = false
+    quit_window.visible = false
+    get_tree().paused = false
