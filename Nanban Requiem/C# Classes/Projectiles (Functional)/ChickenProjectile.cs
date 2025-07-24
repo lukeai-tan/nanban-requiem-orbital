45bf6201224db6f75f612cda7e80808aaf376192
lukeai-tan <lukeaidantan1612@gmail.com>
2025-07-24 22:05:39 +0800

    created new projectile for chicken don

---
+++ Nanban Requiem/C# Classes/Projectiles (Functional)/ChickenProjectile.cs
@@ -0,0 +1,33 @@
+using System;
+using Godot;
+
+public partial class ChickenProjectile : EnemyProjectileA
+{
+    private float timeSinceSpawn = 0f;
+    private float timeToLand = 2f;
+
+
+    public override void _Ready()
+    {
+        AnimatedSprite2D animation = GetNodeOrNull<AnimatedSprite2D>("AnimatedSprite2D");
+        animation.Visible = false;
+        base._Ready();
+    }
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if (!this.initialized || this.target == null)
+        {
+            return;
+        }
+        this.GlobalPosition = this.target.GlobalPosition;
+        AnimatedSprite2D animation = GetNodeOrNull<AnimatedSprite2D>("AnimatedSprite2D");
+        animation.Visible = true;
+        timeSinceSpawn += (float)delta;
+        if (timeSinceSpawn >= timeToLand)
+        {
+            this.Land();
+        }
+    }
+}
